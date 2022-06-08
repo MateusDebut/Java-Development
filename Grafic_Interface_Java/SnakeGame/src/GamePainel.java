53099b2d341afb64e21a2eceef502d6ac1152b2a
MateusDebut <mateusdossantosribeiro100@usp.br>
2022-06-08 20:55:16 -0300

    Criação do painel do jogo

--- Grafic_Interface_Java/SnakeGame/src/GamePainel.java
+++ Grafic_Interface_Java/SnakeGame/src/GamePainel.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
+import java.io.Serializable;
 
-public class GamePainel extends JPanel {
+public class GamePainel extends JPanel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    public static final int WIDTH = 500;
+    public static final int HEIGHT = 500;
     public GamePainel(){
-
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
     }
 
 }
