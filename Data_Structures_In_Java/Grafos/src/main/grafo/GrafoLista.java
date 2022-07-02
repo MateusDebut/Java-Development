c88e186c209e80f2c47a7b89cf67d00fcaa2f4e4
MateusDebut <mateusdossantosribeiro100@usp.br>
2022-07-02 15:35:45 -0300

    Criação do Grafo com Lista de Adjacencias

---
+++ Data_Structures_In_Java/Grafos/src/main/grafo/GrafoLista.java
@@ -0,0 +1,30 @@
+package main.grafo;
+
+import java.util.List;
+
+public class GrafoLista extends DigrafoLista{
+
+    public GrafoLista(List<Vertice> vertices) {
+        super(vertices);
+    }
+
+    @Override
+    public void adicionarAresta(Vertice origem, Vertice destino) {
+        int indiceOrigem = vertices.indexOf(origem);
+        int indiceDestino = vertices.indexOf(destino);
+        listaDeAdjacencia.get(indiceOrigem).add(new Aresta(1, destino));
+        listaDeAdjacencia.get(indiceDestino).add(new Aresta(1, origem));
+    }
+
+    @Override
+    public void removerAresta(Vertice origem, Vertice destino) {
+        super.removerAresta(origem, destino);
+        super.removerAresta(destino, origem);
+    }
+
+    @Override
+    public boolean possuiAlgumaAresta(Vertice vertice) {
+        int indiceVertice = vertices.indexOf(vertice);
+        return !listaDeAdjacencia.get(indiceVertice).isEmpty();
+    }
+}
