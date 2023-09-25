12712b0348b0fd6cf33b31838ef1f58ef4cf557b
MateusDebut <mateusdossantosribeiro100@usp.br>
2023-09-24 21:50:06 -0300

    estabelecendo conexão tcp

---
+++ Parallel-Programming-Java/servidor-tarefas/src/cliente/ClienteTarefas.java
@@ -0,0 +1,14 @@
+package cliente;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClienteTarefas {
+
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 12345);
+        System.out.println("Conexão estabelecida");
+        socket.close();
+    }
+}
